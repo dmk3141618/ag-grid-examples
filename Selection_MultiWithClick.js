a5ef2e44f2c757ff7436a1139d7b05abe762cbc6
Jack Kim <81060569+dmk3141618@users.noreply.github.com>
2022-08-29 23:58:15 +0900

    Create Selection_MultiWithClick.js

---
+++ Selection_MultiWithClick.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+class GridExample extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      columnDefs: [
+        { field: 'athlete', minWidth: 150 },
+        { field: 'age', maxWidth: 90 },
+        { field: 'country', minWidth: 150 },
+        { field: 'year', maxWidth: 90 },
+        { field: 'date', minWidth: 150 },
+        { field: 'sport', minWidth: 150 },
+        { field: 'gold' },
+        { field: 'silver' },
+        { field: 'bronze' },
+        { field: 'total' },
+      ],
+      defaultColDef: {
+        flex: 1,
+        minWidth: 100,
+      },
+      rowSelection: 'multiple',
+      rowData: null,
+    };
+  }
+
+  onGridReady = (params) => {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    const updateData = (data) => params.api.setRowData(data);
+
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .then((resp) => resp.json())
+      .then((data) => updateData(data));
+  };
+
+  render() {
+    return (
+      <div style={{ width: '100%', height: '100%' }}>
+        <div
+          style={{
+            height: '100%',
+            width: '100%',
+          }}
+          className="ag-theme-alpine"
+        >
+          <AgGridReact
+            columnDefs={this.state.columnDefs}
+            defaultColDef={this.state.defaultColDef}
+            rowSelection={this.state.rowSelection}
+            rowMultiSelectWithClick={true}
+            onGridReady={this.onGridReady}
+            rowData={this.state.rowData}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<GridExample></GridExample>, document.querySelector('#root'));
