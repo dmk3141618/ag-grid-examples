53cd38199a8826c1c82271850288b733e1cfa59c
Jack Kim <81060569+dmk3141618@users.noreply.github.com>
2022-08-29 23:59:22 +0900

    Create Selection_DisplayingDisabledCheckboxes.js

---
+++ Selection_DisplayingDisabledCheckboxes.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-enterprise';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+class GridExample extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      columnDefs: [
+        {
+          field: 'athlete',
+          headerCheckboxSelection: true,
+          checkboxSelection: true,
+          showDisabledCheckboxes: true,
+        },
+        { field: 'sport' },
+        { field: 'year', maxWidth: 120 },
+      ],
+      defaultColDef: {
+        flex: 1,
+        minWidth: 100,
+      },
+      rowSelection: 'multiple',
+      isRowSelectable: (params) => {
+        return !!params.data && params.data.year === 2012;
+      },
+      rowData: null,
+    };
+  }
+
+  onGridReady = (params) => {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    const updateData = (data) => params.api.setRowData(data);
+
+    fetch('https://www.ag-grid.com/example-assets/small-olympic-winners.json')
+      .then((resp) => resp.json())
+      .then((data) => updateData(data));
+  };
+
+  onFirstDataRendered = (params) => {
+    params.api.forEachNode((node) =>
+      node.setSelected(!!node.data && node.data.year === 2012)
+    );
+  };
+
+  render() {
+    return (
+      <div style={{ width: '100%', height: '100%' }}>
+        <div
+          style={{
+            height: '100%',
+            width: '100%',
+          }}
+          className="ag-theme-alpine"
+        >
+          <AgGridReact
+            columnDefs={this.state.columnDefs}
+            defaultColDef={this.state.defaultColDef}
+            rowSelection={this.state.rowSelection}
+            suppressRowClickSelection={true}
+            isRowSelectable={this.state.isRowSelectable}
+            onGridReady={this.onGridReady}
+            onFirstDataRendered={this.onFirstDataRendered.bind(this)}
+            rowData={this.state.rowData}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<GridExample></GridExample>, document.querySelector('#root'));
